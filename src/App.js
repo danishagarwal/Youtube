27cd3c7cd1fd5861d39c7c2570d81b7077d845c0
Danish Agarwal <danishagarwal9@gmail.com>
2023-02-25 12:53:20 +0530

    tailwindConfig

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <h1 className="text-xs">Hello World</h1>
     </div>
   );
 }
