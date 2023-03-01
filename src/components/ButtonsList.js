00e74c59d27aed3d91bb6fe0ba18da7e3e0a8330
Danish Agarwal <danishagarwal9@gmail.com>
2023-03-01 14:10:53 +0530

    body_button

--- src/components/ButtonsList.js
+++ src/components/ButtonsList.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Button from './Button'
+
+const list = ["All", "Gaming", "Live", "TV", "Movies", "Development", "Cricket", "Valentines", "Cooking", "Guitar", "Game shows", "Mantras"];
 
 const ButtonsList = () => {
     return (
-        <div>ButtonsList</div>
+        <div className='flex'>
+            {list.map((buttonn) => <Button name={buttonn}></Button>)}
+        </div>
 
     )
 }
