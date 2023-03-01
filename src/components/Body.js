95b656c22adf18df25d266134d19822e21471e59
Danish Agarwal <danishagarwal9@gmail.com>
2023-03-01 21:03:03 +0530

    configure_router

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom';
 import MainContainer from './MainContainer';
 import Sidebar from './Sidebar'
 
@@ -6,7 +7,9 @@ const Body = () => {
     return (
         <div className='flex'>
             <Sidebar />
-            <MainContainer />
+
+            {/* We either want all our videos to be displayed or our watchPage hence outlet -> It will take body's childern*/}
+            <Outlet />
         </div>
     )
 }
