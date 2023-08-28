6bb11b314fb1298afb25ed51c902eff5d9465f65
Danish Agarwal <danishagarwal9@gmail.com>
2023-08-28 11:17:35 +0530

    Truncate title

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -6,11 +6,23 @@ const VideoCard = ({ info }) => {
   const { snippet, statistics } = info;
   const { channelTitle, title, thumbnails } = snippet;
 
+  //To show '...' in the title if length is very long
+  function truncateString(str, maxLength) {
+    if (str.length > maxLength) {
+      return str.substring(0, maxLength) + "...";
+    }
+    return str;
+  }
+  const maxLength = 50;
+
+  const truncatedString = truncateString(title, maxLength);
+  console.log(truncatedString);
+
   return (
     <div className="p-2 m-2 w-72 h-80 shadow-lg">
       <img src={thumbnails.medium.url} alt="thumbnail" className="rounded-lg" />
       <ul>
-        <li className="font-bold py-2">{title}</li>
+        <li className="font-bold py-2">{truncatedString}</li>
         <li>{channelTitle}</li>
         <li>{getViews(statistics.viewCount)} views</li>
       </ul>
